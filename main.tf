5a12c6a7172a43f8ffa744f19ed5456cf3af1aad
gregkroon <44827446+gregkroon@users.noreply.github.com>
2023-11-24 13:41:48 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "harness_platform_gitops_cluster" "gitopscluster" {
 
 
 depends_on = [
-    module.irsa-ebs-csi.ebs-csi
+    module.irsa-ebs-csi.aws_eks_addon.ebs-csi
  ]
   identifier = "argocluster"
   account_id = "Ke-E1FX2SO2ZAL2TXqpLjg"
@@ -153,4 +153,52 @@ depends_on = [
       request.0.upsert, request.0.cluster.0.config.0.bearer_token,
     ]
   }
+}
+resource "harness_platform_gitops_applications" "example" {
+
+depends_on = [
+    harness_platform_gitops_cluster.gitopscluster
+ ]
+  application {
+    metadata {
+      annotations = {}
+      labels = {
+        "harness.io/serviceRef" = "service_id"
+        "harness.io/envRef"     = "env_id"
+      }
+      name = "appname123"
+    }
+    spec {
+      sync_policy {
+        sync_options = [
+          "PrunePropagationPolicy=undefined",
+          "CreateNamespace=false",
+          "Validate=false",
+          "skipSchemaValidations=false",
+          "autoCreateNamespace=false",
+          "pruneLast=false",
+          "applyOutofSyncOnly=false",
+          "Replace=false",
+          "retry=false"
+        ]
+      }
+      source {
+        target_revision = "master"
+        repo_url        = "https://github.com/willycoll/argocd-example-apps.git"
+        path            = "helm-guestbook"
+
+      }
+      destination {
+        namespace = "namespace-123"
+        server    = "https://1.3.4.5"
+      }
+    }
+  }
+  project_id = "project_id"
+  org_id     = "org_id"
+  account_id = "account_id"
+  identifier = "identifier"
+  cluster_id = "cluster_id"
+  repo_id    = "repo_id"
+  agent_id   = "agent_id"
 }
