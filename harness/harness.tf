f2947099c754ba137b832d6f730fdc5ff74cfd39
gregkroon <44827446+gregkroon@users.noreply.github.com>
2023-11-27 21:56:16 +1100

    Update harness.tf

--- harness/harness.tf
+++ harness/harness.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+    }
+  }
+}
+
+
+
 provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = var.accountid
