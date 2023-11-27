70455e01e3eae5c1c9d5dcb94b95f00c8eb88933
gregkroon <44827446+gregkroon@users.noreply.github.com>
2023-11-27 22:11:38 +1100

    Update terraform.tf

--- harness/terraform.tf
+++ harness/terraform.tf
@@ -1,6 +1,25 @@
 terraform {
   required_providers {
-    
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
      harness = {
       source = "harness/harness"
     }
