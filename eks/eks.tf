7a8a16aa80dbf321cb88ae583bf0e80046233c91
gregkroon <munky_s@hotmail.com>
2023-11-27 19:36:56 +1100

    restructure

---
+++ eks/eks.tf
@@ -0,0 +1,256 @@
+
+
+provider "aws" {
+  region = var.region
+}
+
+provider "harness" {
+  endpoint         = "https://app.harness.io/gateway"
+  account_id       = var.accountid
+  platform_api_key = var.apikey
+}
+
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = var.clustername
+}
+
+output "agent_token" {
+  value = harness_platform_gitops_agent.gitopseks.agent_token
+
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "education-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
+} 
+
+resource "harness_platform_gitops_agent" "gitopseks" {
+  identifier = "gitopseks"
+  account_id = "Ke-E1FX2SO2ZAL2TXqpLjg"
+  project_id = "CANVA"
+  org_id     = "default"
+  name       = "gitopseks"
+  type       = "MANAGED_ARGO_PROVIDER"
+  metadata {
+    namespace         = "default"
+    high_availability = true
+  }
+}
+
+resource "harness_platform_gitops_cluster" "gitopscluster" {
+
+
+  identifier = "argocluster"
+  account_id = "Ke-E1FX2SO2ZAL2TXqpLjg"
+  project_id = "CANVA"
+  org_id     = "default"
+  agent_id   = "gitopseks"
+
+
+  request {
+    upsert = false
+    cluster {
+      server = "https://kubernetes.default.svc"
+      name   = "argocluster"
+      config {
+        tls_client_config {
+          insecure = true
+        }
+        cluster_connection_type = "IN_CLUSTER"
+      }
+
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      request.0.upsert, request.0.cluster.0.config.0.bearer_token,
+    ]
+  }
+}
+
+
+resource "harness_platform_gitops_repository" "gitrepo" {
+
+  depends_on = [
+    harness_platform_gitops_cluster.gitopscluster
+  ]
+  identifier = "gitrepo"
+  account_id = "Ke-E1FX2SO2ZAL2TXqpLjg"
+  project_id = "CANVA"
+  org_id     = "default"
+  agent_id   = "gitopseks"
+  repo {
+    repo            = "https://github.com/argoproj/argocd-example-apps"
+    name            = "gitrepo"
+    insecure        = true
+    connection_type = "HTTPS_ANONYMOUS"
+  }
+  upsert = true
+}
+
+
+
+resource "harness_platform_gitops_applications" "gitopsapplication" {
+  depends_on = [
+    harness_platform_gitops_repository.gitrepo
+  ]
+
+  application {
+    metadata {
+      annotations = {}
+      labels = {
+        "harness.io/serviceRef" = "guestbook"
+        "harness.io/envRef"     = "dev"
+      }
+      name = "guestbook"
+      
+    }
+
+    spec {
+      sync_policy {
+        sync_options = [
+          "PrunePropagationPolicy=undefined",
+          "CreateNamespace=false",
+          "Validate=false",
+          "skipSchemaValidations=false",
+          "autoCreateNamespace=false",
+          "pruneLast=false",
+          "applyOutofSyncOnly=false",
+          "Replace=false",
+          "retry=false"
+        ]
+      }
+      
+      
+
+      source {
+        target_revision = "master"
+        repo_url        = "https://github.com/argoproj/argocd-example-apps"
+        path            = "helm-guestbook"
+        helm {
+          value_files = ["values.yaml"]
+              }
+        
+      }
+      destination {
+        namespace = "default"
+        server    = "https://kubernetes.default.svc"
+      }
+    }
+  }
+
+  name       = "guestbook"
+  project_id = "CANVA"
+  org_id     = "default"
+  account_id = "Ke-E1FX2SO2ZAL2TXqpLjg"
+  cluster_id  = "argocluster"
+  repo_id     = "gitrepo"
+  agent_id    = "gitopseks"
+}
+
+
+
+
